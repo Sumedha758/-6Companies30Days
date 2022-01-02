6219e01f43250ba0757246527af77106e2cebbef
Sumedha Mishra <67469667+Sumedha758@users.noreply.github.com>
2022-01-02 16:11:36 +0530

    Create Nth Ugly Number.java

---
+++ Nth Ugly Number.java
@@ -0,0 +1,24 @@
+class Solution {
+    
+    public int nthUglyNumber(int n) {
+        int dp[][]=new int[3][n+1];
+        int res=1;
+        int a=0, b=0, c=0;
+        for(int i=0; i<n-1; i++){
+            dp[0][i]=res*2;
+            dp[1][i]=res*3;
+            dp[2][i]=res*5;
+            res =Math.min(dp[0][a], Math.min(dp[1][b], dp[2][c]));
+            if(res == dp[0][a]){
+                a++;
+            }
+            if(res == dp[1][b]){
+                b++;
+            }
+            if(res == dp[2][c]){
+                c++;
+            }
+        }
+        return res;
+    }
+}
